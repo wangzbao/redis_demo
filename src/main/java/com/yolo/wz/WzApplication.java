75eae3b7cc7e7a8d7e6c1b5902238fd423d06148
unknown <wangzhan>
2021-04-22 21:50:33 +0800

    first commit

---
+++ src/main/java/com/yolo/wz/WzApplication.java
@@ -0,0 +1,13 @@
+package com.yolo.wz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WzApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yolo/wz/WzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yolo.wz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
