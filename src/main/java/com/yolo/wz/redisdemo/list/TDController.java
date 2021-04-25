f7ff4ca55e3229520db27e052da96c62dd746711
wangzhan <wangzhanazn@163.com>
2021-04-25 23:26:13 +0800

    测试git上传

--- src/main/java/com/yolo/wz/redisdemo/list/TDController.java
+++ src/main/java/com/yolo/wz/redisdemo/list/TDController.java
@@ -1,6 +1,8 @@
 package com.yolo.wz.redisdemo.list;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,14 +13,21 @@ public class TDController {
     @Value("${td}")
     private String td;
 
+    @Autowired
+    private RedisTemplate redisTemplate = new RedisTemplate();
 
     @GetMapping("get")
-    public String getTDDoc(){
-        System.out.println("coming.....");
-        System.out.println(td);
+    public String getTDDoc() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i+1);
+            redisTemplate.boundListOps("key").rightPush("第" + (i + 1) + "个v");
+        }
+        for (int i = 0; i < 10; i++) {
+            String v = (String) redisTemplate.boundListOps("key").leftPop();
+            System.out.println(v);
+        }
+
         return td;
     }
-
-    
 
 }
--- src/main/java/com/yolo/wz/redisdemo/set/MicroBlogDemo.java
+++ src/main/java/com/yolo/wz/redisdemo/set/MicroBlogDemo.java
@@ -2,17 +2,78 @@ package com.yolo.wz.redisdemo.set;
 
 import redis.clients.jedis.Jedis;
 
+import java.util.Set;
+
 /**
  * 微博案例
  */
 public class MicroBlogDemo {
     private Jedis jedis = new Jedis("127.0.0.1");
 
+    /**
+     * 关注用户
+     */
     public void follow(long userId, long followUserId) {
         //大v的粉丝列表增加用户
-        jedis.sadd("user::"+followUserId+"::followers",String.valueOf(userId));
+        jedis.sadd("user::" + followUserId + "::followers", String.valueOf(userId));
         //用户的关注列表增加一个大v
-        jedis.sadd("user::"+userId+"::followe_users",String.valueOf(followUserId));
+        jedis.sadd("user::" + userId + "::followe_users", String.valueOf(followUserId));
+    }
+
+    /**
+     * 取消关注用户
+     */
+    public void unfollow(long userId, long followUserId) {
+        //大v的粉丝列表减少用户
+        jedis.srem("user::" + followUserId + "::followers", String.valueOf(userId));
+        //用户的关注列表减少一个大v
+        jedis.srem("user::" + userId + "::followe_users", String.valueOf(followUserId));
+    }
+
+    /**
+     * 查看有哪些人关注了自己
+     */
+    public Set<String> getWhoFollowMe(long userId) {
+        return jedis.smembers("user::" + userId + "::followers");
+    }
+
+    /**
+     * 查看关注了自己的人数
+     */
+    public long getFollowsCount(long userId) {
+        return jedis.scard("user::" + userId + "::followers");
+    }
+
+    /**
+     * 查看自己关注了哪些人
+     */
+    public Set<String> getFollowsUser(long userId) {
+        return jedis.smembers("user::" + userId + "::followe_users");
+    }
+
+    public static void main(String[] args) {
+        MicroBlogDemo demo = new MicroBlogDemo();
+        long userId = 1;
+        long friendId = 2;
+        long superStar = 3;
+
+        demo.follow(userId, superStar);
+        demo.follow(friendId, superStar);
+        demo.follow(userId, friendId);
+
+        //明星查看他被谁关注了
+        Set<String> superStarsFans = demo.getWhoFollowMe(superStar);
+        System.out.println("明星的粉丝：" + superStarsFans.toString());
+        System.out.println("明星的粉丝数量：" + demo.getFollowsCount(superStar));
+
+        //朋友看看自己被谁关注了，自己关注了谁
+        System.out.println("朋友看看被谁关注了：" + demo.getWhoFollowMe(friendId));
+        System.out.println("朋友看看关注了谁：" + demo.getFollowsUser(friendId));
+        System.out.println("朋友看看被关注的人数：" + demo.getFollowsCount(friendId));
+
+        //我关注了谁
+        System.out.println("查看我关注了谁：" + demo.getFollowsUser(userId));
+        System.out.println("查看我关注的人数：" + demo.getFollowsCount(userId));
     }
 
 }
